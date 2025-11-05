b23583c856249a3f87649ee02261d5ce5eadf43c
YAN SILVA LEAL <yan.sleal@senacsp.edu.br>
2025-11-04 21:01:31 -0300

    feat: configuração do DB mysql

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,9 @@ import { Module } from '@nestjs/common';
 
 import { QueroAjudarModule } from './modules/FormularioQueroAjudar/queroAjudar.module';
 import { PrecisoAjudaModule } from './modules/FormularioPrecisoAjuda/precisoAjuda.module';
+import { databaseModule } from 'src/database/database.module';
 
 @Module({
-  imports: [PrecisoAjudaModule, QueroAjudarModule],
+  imports: [databaseModule, PrecisoAjudaModule, QueroAjudarModule],
 })
 export class AppModule {}
